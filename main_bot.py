d7694d41fd614677c2da003d568135ddbac35616
AndreyKlimmov <andrey.klimmov@gmail.com>
2022-09-10 19:10:37 +0300

    still static and no functions

--- main_bot.py
+++ main_bot.py
@@ -18,8 +18,6 @@ users = [{
     "name": "Petr"
     }]
 
-error_message = ["Not found", "Incorrect input"]
-
 sentences = [
     {"text": "Most sentences contain a subject and a verb",
     "level": 0},
@@ -35,7 +33,6 @@ sentences = [
     "level": 0}
 ]
 
-
 TOKEN = '5450847244:AAHMfh_VpngOG9t6LJWGNhBVwH1z21F1y_g'
 
 ROOT_URL = f'https://api.telegram.org/bot{TOKEN}'
@@ -43,7 +40,6 @@ ROOT_URL = f'https://api.telegram.org/bot{TOKEN}'
 def get_update(url, update_id):
     responce = requests.get(f'{url}/getUpdates?offset={update_id + 1}')
     return responce.json()
-
 
 def send_message(chat_id, message_text, url):
     data = {'chat_id': chat_id, 'text': message_text}
@@ -72,18 +68,7 @@ while True:
         for i in output_sentences:
             message_text = message_text + i + '\n'
 
-
     if update_id != answered_update_id:
         chat_id = updates.get('result')[0]['message']['chat']['id']
         send_message(chat_id, message_text, ROOT_URL)
         answered_update_id = update_id
-
-# while True:
-#     updates = get_update(ROOT_URL, answered_update_id)
-#     if updates.get('result'):
-#         update_id = updates.get('result')[0]['update_id']
-#         if update_id != answered_update_id:
-#             chat_id = updates.get('result')[0]['message']['chat']['id']
-#             message_text = updates.get('result')[0]['message']['text']
-#             send_message(chat_id,message_text,ROOT_URL)
-#             answered_update_id = update_id
